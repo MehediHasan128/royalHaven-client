c35faf17bda4a9e94cd71f7f4b03967385171365
MehediHasan128 <mehedihasan12926@gmail.com>
2025-02-15 21:10:17 +0600

    basic project setup complete

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div>
+      <h1 className="text-4xl">This is my first project</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
