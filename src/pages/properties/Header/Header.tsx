ef604c5fceb00c4841d49a282bebc481b095b3a5
MehediHasan128 <mehedihasan12926@gmail.com>
2025-02-21 20:03:54 +0600

    conditional button to get different product

--- src/pages/properties/Header/Header.tsx
+++ src/pages/properties/Header/Header.tsx
@@ -1,12 +1,26 @@
 import { Typography } from '@mui/material';
 import logo from '../../../assets/images/logo.png';
+import { useState } from 'react';
+
+
+const buttons = ["All", "Sell", "Rent", "Favorites", "Services"];
+
 
 const Header = () => {
+    const [activeBtn, setActiveBtn] = useState("All");
+
     return (
-        <div>
+        <div className='flex items-center justify-between w-full'>
             <div className='w-fit flex items-center gap-3'>
                 <img className='w-8' src={logo} alt="" />
                 <Typography variant='h5'><span id='logoFont'>Royal Haven</span></Typography>
+            </div>
+            <div className='flex justify-between items-center gap-2 p-1 rounded-full bg-gray-100'>
+                {
+                    buttons.map((btn) => (
+                        <button onClick={() => setActiveBtn(btn)} key={btn} className={`${(activeBtn === btn)? "bg-white text-red-600 font-medium" : "bg-transparent text-black"} px-4 py-1 rounded-full cursor-pointer`}><Typography variant='caption'>{btn}</Typography></button>
+                    ))
+                }
             </div>
         </div>
     );
